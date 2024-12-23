11e3a253b5af72e38041b3befb9874132e729c90
lukegeorgeson9 <38395511-lukegeorgeson9@users.noreply.replit.com>
2024-12-23 16:33:29 +0000

    Restored to '40a63ba4a385b862d7831378a07b81ba2f4614ea'
    Replit-Restored-To:40a63ba4a385b862d7831378a07b81ba2f4614ea

--- client/src/types/gallery.ts
+++ client/src/types/gallery.ts
@@ -4,8 +4,6 @@ export interface Image {
   url: string;
   starred?: boolean;
   commentCount?: number;
-  approved?: boolean;
-  reviewed?: boolean;
   position?: number;
   originalFilename?: string;
   width?: number;
