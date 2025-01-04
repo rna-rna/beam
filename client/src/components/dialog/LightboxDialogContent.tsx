1320060cd57b2f92eea670aa948ed840eba95101
Luke Georgeson <38395511-lukegeorgeson9@users.noreply.replit.com>
2025-01-04 13:54:55 +0000

    Assistant checkpoint: Add ESC key handler to close lightbox dialog
    Assistant generated file changes:
    - client/src/components/dialog/LightboxDialogContent.tsx: Add ESC key handler to close lightbox, Update LightboxDialogContentProps interface
    
    ---
    
    User prompt:
    
    pressing esc should close lightboxdialogcontent

--- client/src/components/dialog/LightboxDialogContent.tsx
+++ client/src/components/dialog/LightboxDialogContent.tsx
@@ -3,23 +3,37 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 interface LightboxDialogContentProps
-  extends React.HTMLAttributes<HTMLDivElement> {}
+  extends React.HTMLAttributes<HTMLDivElement> {
+  onOpenChange?: (open: boolean) => void;
+}
 
 const LightboxDialogContent = React.forwardRef<
   HTMLDivElement,
   LightboxDialogContentProps
->(({ className, children, ...props }, ref) => (
-  <div
-    ref={ref}
-    {...props}
-    className={cn(
-      "fixed inset-0 w-screen h-screen z-50 flex items-center justify-center bg-black/100 p-0",
-      className
-    )}
-  >
-    {children}
-  </div>
-));
+>(({ className, children, ...props }, ref) => {
+  React.useEffect(() => {
+    const handleEscKey = (e: KeyboardEvent) => {
+      if (e.key === "Escape" && props.onOpenChange) {
+        props.onOpenChange(false);
+      }
+    };
+    window.addEventListener("keydown", handleEscKey);
+    return () => window.removeEventListener("keydown", handleEscKey);
+  }, [props.onOpenChange]);
+
+  return (
+    <div
+      ref={ref}
+      {...props}
+      className={cn(
+        "fixed inset-0 w-screen h-screen z-50 flex items-center justify-center bg-black/100 p-0",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+});
 
 LightboxDialogContent.displayName = "LightboxDialogContent";
 
