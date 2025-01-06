32656ffbe7dd6a7292f57822797dae324a7a11ae
Luke Georgeson <38395511-lukegeorgeson9@users.noreply.replit.com>
2025-01-06 08:34:22 +0000

    Restored to 'e49467de403f80a61c95b1d25f0f1c9b2f75fb7e'
    Replit-Restored-To:e49467de403f80a61c95b1d25f0f1c9b2f75fb7e

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,23 @@
-
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
-import path from "path";
+import path, { dirname } from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-import { createHtmlPlugin } from 'vite-plugin-html';
+import { fileURLToPath } from "url";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [
-      react(),
-      runtimeErrorOverlay(),
-      themePlugin(),
-      createHtmlPlugin({
-        inject: {
-          data: {
-            title: 'Gallery',
-            description: 'Powered by Beam',
-            ogImage: env.VITE_DEFAULT_OG_IMAGE,
-            url: env.VITE_SITE_URL,
-          },
-        },
-      }),
-    ],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './client/src')
-      }
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+export default defineConfig({
+  plugins: [react(), runtimeErrorOverlay(), themePlugin()],
+  resolve: {
+    alias: {
+      "@db": path.resolve(__dirname, "db"),
+      "@": path.resolve(__dirname, "client", "src"),
     },
-    build: {
-      outDir: path.resolve(__dirname, "dist/public"),
-      emptyOutDir: true,
-    }
-  };
+  },
+  root: path.resolve(__dirname, "client"),
+  build: {
+    outDir: path.resolve(__dirname, "dist/public"),
+    emptyOutDir: true,
+  },
 });
