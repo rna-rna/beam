ea9e8659fad443ebb0a298db9c8bb7600db376ee
lukegeorgeson9 <38395511-lukegeorgeson9@users.noreply.replit.com>
2025-01-16 18:42:39 +0000

    User checkpoint: Update Dashboard sidebar import.

--- client/src/components/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-
-// Update Dashboard.tsx import
-<old_str>
-import { DashboardSidebarV2 } from "@/components/DashboardSidebarV2";
-</old_str>
-<new_str>
-import { DashboardSidebar } from "@/components/DashboardSidebar";
-</new_str>
